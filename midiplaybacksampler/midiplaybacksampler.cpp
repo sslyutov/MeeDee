c534d146d2c89ee87c4791e08a728bf68be274e3
Sergey Slyutov <sslyutov@gmail.com>
2025-01-25 19:55:18 -0800

    new files for midiplaybacksampler

---
+++ midiplaybacksampler/midiplaybacksampler.cpp
@@ -0,0 +1,8 @@
+
+/*!\brief combination of midi source and audio sampler
+ * \file midiplaybacksampler.cpp
+ * \author Sergey Slyutov
+ * \date January 25, 2025
+ */
+
+#include "midiplaybacksampler.h"
---
+++ midiplaybacksampler/midiplaybacksampler.h
@@ -0,0 +1,13 @@
+/*!\brief combination of midi source and audio sampler
+ * \file midiplaybacksampler.h
+ * \author Sergey Slyutov
+ * \date January 25, 2025
+ */
+    
+#ifndef _MIDIPLAYBACKSAMPLER_H
+
+#define _MIDIPLAYBACKSAMPLER_H
+
+
+
+#endif // _MIDIPLAYBACKSAMPLER_H
